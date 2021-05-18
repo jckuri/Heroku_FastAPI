6f701f94ffbaf760e8d74e7f285665d591f3bc48
jckuri <jckuri@gmail.com>
2021-05-18 00:21:05 -0500

    Second commit.

--- test_functions.py
+++ test_functions.py
@@ -1,2 +1,20 @@
-def test_first_test():
-    assert 2 + 2 == 4
+import pandas
+import starter.starter.ml.data as ml_data
+import starter.starter.ml.model as ml_model
+import starter.starter.train_model as train_model
+
+
+def test_load_dataset():
+    df = train_model.load_dataset(train_model.DATA_FILE)
+    assert type(df) == pandas.DataFrame
+    assert df.shape == (32561, 15)
+
+
+def test_split_dataset():
+    df = train_model.load_dataset(train_model.DATA_FILE)
+    train_dataset, test_dataset = train_model.split_dataset(df)
+    assert type(train_dataset) == pandas.DataFrame
+    assert type(test_dataset) == pandas.DataFrame
+    assert train_dataset.shape == (26048, 15)
+    assert test_dataset.shape == (6513, 15)
+    
